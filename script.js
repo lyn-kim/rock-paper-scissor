9541cbdc140e564bcc884c740e56fadb038c96c3
Lyn Kim <lynkim216@outlook.com>
2021-10-13 14:40:36 -0700

    define variables for scoring

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ function computerPlay() {
     return randomMove;
 };
 
-const playerSelection = 'Rock';
-const computerSelection = computerPlay();
-
 function playRound(playerSelection, computerSelection) {
-    if ('Rock', computerPlay() === 'Paper') {
-        return "You Lose! Paper beats Rock";
-    } else if ('Rock', computerPlay() === 'Rock') {
-        return "You tied! Rock does not beat Rock";
+    if (playerSelection, computerPlay() === 'Paper') {
+        computerScore++;
+        return lose;
+    } else if (playerSelection, computerPlay() === 'Rock') {
+        return tie;
     } else { 
-        return "You won! Rock beats Scissor";
+        playerScore++;
+        return win;
     }
 }
 
+function game() {
+    
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let win = "You won! Rock beats Scissor.";
+let lose = "You lost! Paper beats Rock.";
+let tie = "It's a tie! Rock does not beat Rock.";
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+console.log("Player score is " + playerScore + ".", "Computer score is " + computerScore + ".");
