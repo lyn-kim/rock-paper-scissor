98d36a752ec77908396cac329256366d4647a544
Lyn Kim <lynkim216@outlook.com>
2021-10-12 18:16:11 -0700

    add playRound function

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function computerPlay() {
     let randomMove = move[Math.floor(Math.random() * move.length)];
     return randomMove;
 };
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    if ('Rock', computerPlay() === 'Paper') {
+        return "You Lose! Paper beats Rock";
+    } else if ('Rock', computerPlay() === 'Rock') {
+        return "You tied! Rock does not beat Rock";
+    } else { 
+        return "You won! Rock beats Scissor";
+    }
+}
+
